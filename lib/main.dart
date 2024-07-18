fd3ddbe4df8dd7450aa5ee58062b934cc3e03b88
Gernot Doriat <gernot.doriat@web.de>
2024-07-18 17:03:46 +0200

    ZWS

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,77 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, avoid_print
+
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:docx_to_text/docx_to_text.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
+  runApp(MyApp());
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'File Picker Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _filePath = 'Keine Datei ausgewählt';
+
+  Future<void> _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null && result.files.single.path != null) {
+      setState(() async {
+        _filePath = result.files.single.path!;
+        debugPrint(_filePath);
+        /*     final file = File(_filePath);
+        final bytes = await file.readAsBytes();
+        final text = docxToText(bytes, handleNumbering: true);
+        debugPrint(text); */
+      });
+    } else {
+      // Benutzer hat die Datei-Auswahl abgebrochen
+      setState(() {
+        _filePath = 'Keine Datei ausgewählt';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text('The App Bar!'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]),
-      body: Row(
-        children: <Widget>[
-          Expanded(child: Image.asset('lib/assets/image.jpeg')),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      padding: EdgeInsets.all(30.0),
-                      color: Colors.cyan,
-                      child: Text('1'),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      padding: EdgeInsets.all(30.0),
-                      color: Colors.pinkAccent,
-                      child: Text('2'),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      padding: EdgeInsets.all(30.0),
-                      color: Colors.amber,
-                      child: Text('3'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+        title: Text('File Picker Demo'),
       ),
-      floatingActionButton: FloatingActionButton(
-        // ignore: avoid_print
-        onPressed: () => {print('FloatingActionButton')},
-        backgroundColor: Colors.red[600],
-        child: const Text('click'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: _pickFile,
+              child: Text('Datei auswählen'),
+            ),
+            SizedBox(height: 20),
+            Text('Ausgewählte Datei: $_filePath'),
+          ],
+        ),
       ),
     );
   }
